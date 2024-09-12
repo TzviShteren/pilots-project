a519459fe75ec94e126e49c53321d242f5d91705
tzevi stern <z02072000@gmail.com>
2024-09-12 11:33:26 +0300

    Adding a function of coordinates to JSON

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-from utils import calculations as calcu
+import json
 import requests
+from utils import calculations as calcu
+from model import operation
 
-# Getting the coordinates of each country and entering into a json file
-# The function will only run once so there won't be too many requests to the API
-def getting_coordinates():
-    pass
+My_Api_key = "bf47316e4a70a4097f5c6d36e53530e3"
+
 
 if __name__ == '__main__':
     pass
 
+
+
+
+# Getting the coordinates of each country and entering into a json file
+# The function will only run once so there won't be too many requests to the API
+def getting_coordinates():
+    with open("json_files/targets.json", 'r') as jsonfile:
+        list_of_targets = json.load(jsonfile)
+    list_of_city = list(map(lambda x: x["City"], list_of_targets))
+    list_of_city.append("Jerusalem")
+    city_coordinates = {}
+    for city in list_of_city:
+        API_URL = f'https://api.openweathermap.org/geo/1.0/direct?q={city}&appid={My_Api_key}'
+        result = requests.get(API_URL).json()
+        if result:  # Ensure we received valid data
+            lat = result[0]["lat"]
+            lon = result[0]["lon"]
+            city_coordinates[city] = [lat, lon]
+    with open("json_files/city_coordinates.json", 'w') as jsonfile:
+        json.dump(city_coordinates, jsonfile, indent=4)  # I learned it on my own
---
+++ model/city_coordinates.py
@@ -0,0 +1,6 @@
+class CityCoordinates:
+
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
+
